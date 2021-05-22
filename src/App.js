4d8cc07a06e425bf5712ee725524964fd0bad7cf
MARCELO GOMES CASTANHO <71048436+marcelo-prog@users.noreply.github.com>
2021-05-22 20:22:54 -0300

    20:22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Components from './Components/Components'
 
-function App() {
+export default () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="Inicio">
+      <div>
+        <h1>APOIO AVANÇADO</h1>
+        <Components/>
+      </div>
+    </header>  
   );
-}
 
-export default App;
+} 
---
+++ src/Components/Components.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Components.css';
+
+const Components = () =>{
+  return (
+    <nav className='opcoes'>
+      <div>
+          <ul>
+              <li><a>ÁREA TATICA</a></li>
+              <li><a>ÁREA TÉCNICA</a></li>
+              <li><a>ÁREA COMERCIAL</a></li>
+          </ul>
+       
+      </div>
+    </nav>  
+  );
+
+}
+
+export default Components ;
